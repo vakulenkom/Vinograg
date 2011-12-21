1e6ec75d3f4964cb6a997640c46d866f46116996
mike <vakulenkom@gmail.com>
2011-12-21 22:13:51 +0300

    добавлен счётчик времени для винограда

--- vinograd3/main.cpp
+++ vinograd3/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <ctime>
 
 void vinogradSimpleMult(const double* g, const double* d, double* res)
 { 
@@ -22,23 +23,21 @@ void vinogradSimpleMult(const double* g, const double* d, double* res)
     double c9=(g[0]+g[1]-2*g[2]+g[3])*(d[0]+d[1]-2*d[2]+d[3])/3;
     
     //current vesion of m(x) is (x-1)*x^2*(x^2+1)*(x^2-x+1)
-    // tmp[i] - вспомогательные переменные дл€ уменьшени€ количества умножений 
-    //    double tmp1 = c2 - c3;
-    //    double tmp0 = 3.0 * tmp1 + 4.0 * c6 + c7 - 2.0 * c8 + c9;
-    //    double tmp2 = c0 - 2.0 * c6 - c7;
-    //    double tmp3 = c1 + c5;
-    //    double tmp4 = 2.0 * c4 + c8;
-    //    double tmp5 = -2.0 * tmp1 + tmp2;
+    // L[i] - вспомогательные переменные дл€ уменьшени€ количества умножений 
+    double L0 = 3*c7 + 3*c8;
+    double L1 = c2 - c3;
+    double L2 = c1 - c2 + c3;
+    double L3 = c1 - c4;
     
     //_ASSERT(_CrtCheckMemory());
     
     res[0] = c1;
-    res[1] = -c1 + c2 - c3;
-    res[2] = c0 + c1 - c4 + 2*c6 - 3*c8 + c9;
-    res[3] = c0 - 2*c1 + c2 - c3 - c4 + 2*c5 + 3*c7 + 3*c8 - 2*c9;
-    res[4] = 2*c0 + c1 - c2 + c3 - 3*c7 - 3*c8 + 2*c9;
-    res[5] = c0 - c1 + c4 -2*c6 + 3*c7 + 3*c8 -2*c9;
-    res[6] = c0 + c1 - c2 + c3 + c4 - 2*c5 - 3*c7 + c9;
+    res[1] = -c1 + L1;
+    res[2] = c0 + L3 + 2*c6 - 3*c8 + c9;
+    res[3] = c0 - 2*c1 + L1 - c4 + 2*c5 + L0 - 2*c9;
+    res[4] = 2*c0 + L2 - 3*c7 - 3*c8 + 2*c9;
+    res[5] = c0 - L3 - 2*c6 + L0 -2*c9;
+    res[6] = c0 + L2 + c4 - 2*c5 - 3*c7 + c9;
     
 }
 
@@ -50,13 +49,20 @@ int main (int argc, const char * argv[])
 	double b[4];
 	double c[4];
     
+
+    //что-нить делаем
+    
 	for (i=0;i<4;i++){
 		a[i]=(double) i+1;
 		b[i]=(double) i+4;
         //        std::cout <<"a["<<i<<"]="<<a[i]<<"\n";
         //        std::cout <<"b["<<i<<"]="<<b[i]<<"\n";
 	}
+    
+    clock_t tStart = clock();
     vinogradSimpleMult(a,b,c);
+    clock_t tFinish = clock();
+    std::cout << "time: " << (double)(tStart - tFinish) / CLOCKS_PER_SEC<<" sec\n";
     
     for (i=0;i<7;i++)
         std::cout <<"c["<<i<<"]="<<c[i]<<"\n";
